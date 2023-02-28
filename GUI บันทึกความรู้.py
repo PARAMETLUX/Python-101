a202d5612f11e0b3724e399a205c2022ed2c882b
PARAMETLUX <125622288+PARAMETLUX@users.noreply.github.com>
2023-02-28 21:28:35 +0700

    EP4 read and write CSV file

--- GUI บันทึกความรู้.py
+++ GUI บันทึกความรู้.py
@@ -2,6 +2,23 @@ from tkinter import *
 from tkinter import ttk #theme ของ tk
 from tkinter import messagebox #ทำการเรียก function เเสดงผลกล่องข้อความ
 
+#######################CSV#######################
+
+import csv
+
+def writecsv(datalist):
+    with open('data.csv','a',encoding='utf-8',newline='') as file:
+        fw = csv.writer(file) #fw = file writer
+        fw.writerow(datalist) #datalist = ['pen','pencil','eraser']
+
+def readcsv():
+    with open('data.csv',encoding='utf-8',newline='') as file:
+        fr = csv.reader(file) #fr = file reader
+        data = list(fr)
+    return data
+
+#######################CSV#######################
+
 GUI = Tk() #นี่คือหน้าจอหลักของโปรแกรม
 GUI.title('โปรแกรมบันทึกข้อมูล') #การกำหนดชื่อของโปรแกรม
 GUI.geometry('500x500') #การกำหนดขนาดของหน้าต่างโปรแกรม ก*ย
@@ -33,5 +50,25 @@ FB3 = Frame(GUI)
 FB3.place(x=300,y=300)
 B3 = ttk.Button(FB3,text='ล้างข้อมูล',command=Button3)
 B3.pack(ipadx=3,ipady=3)
+
+#######################section right#######################
+LF1 = ttk.LabelFrame(GUI,text='กรอกข้อมูลที่ต้องการเข้าไป')
+LF1.place(x=100,y=100)
+
+v_data = StringVar() #ตัวแปรพิเศษที่ใช้กับข้อความใน GUI
+E1 = ttk.Entry(LF1,textvariable=v_data,font=('Angsana New',25))
+E1.pack(pady=10,padx=10)
+
+from datetime import datetime
+
+def SaveData():
+    t = datetime.now().strftime('%Y%m%d %H%M%S')
+    data = v_data.get() #ดึงข้อมูลจากตัวเเปร v_data มาใช้งาน
+    text = [data]
+    writecsv(text) #บันทึกลง csv
+    v_data.set('') #เคลียร์ข้อมูลที่อยู่ในช่องกรอก
+
+B1 = ttk.Button(LF1,text='บันทึก',command=SaveData) #ผูก function csv กับปุ่ม Button 1
+B1.pack(ipadx=3,ipady=3) #เเสดงปุ่มบนหน้าต่าง (อยู่กึ่งกลางข้างบนสุด)
 
 GUI.mainloop()
